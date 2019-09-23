1a1779c53f1026a5628bfd32879434d670788a86
Dieter Niklaus <dieter.niklaus@xovis.com>
2019-09-23 19:00:24 +0200

    - FIX: BATT_WARN_THRSHD = 6.5V & BATT_STOP_THRSHD = 6.3 (according to product requirements) - FIX: handle N_ADC_FULLRANGE correctly - CHG: DEFAULT_ASYNC_STATUS_EVAL_TIME = 0 (no wait, requires wiring-timer 2.7.0 or later) - get rid of commented unused code

--- Battery.cpp
+++ Battery.cpp
@@ -7,26 +7,19 @@
 
 #include "Battery.h"
 #include "BatteryImpl.h"
-//#include <DbgTracePort.h>
-//#include <DbgTraceLevel.h>
 
-const float Battery::s_BATT_WARN_THRSHD = 7.5;
-const float Battery::s_BATT_STOP_THRSHD = 6.5;
+const float Battery::s_BATT_WARN_THRSHD = 6.5;
+const float Battery::s_BATT_STOP_THRSHD = 6.3;
 const float Battery::s_BATT_SHUT_THRSHD = 6.1;
 const float Battery::s_BATT_HYST        = 0.3;
 
 BatteryAdapter::BatteryAdapter()
 : m_battery(0)
-//, m_trPort(0)
 { }
 
 void BatteryAdapter::attachBattery(Battery* battery)
 {
   m_battery = battery;
-//  if (0 != m_battery)
-//  {
-//    m_trPort = m_battery->trPort();
-//  }
 }
 
 Battery* BatteryAdapter::battery()
@@ -36,48 +29,32 @@ Battery* BatteryAdapter::battery()
 
 void BatteryAdapter::notifyBattVoltageOk()
 {
-//  if (0 != m_trPort)
-//  {
-//    TR_PRINTF(m_trPort, DbgTrace_Level::info, "BatteryAdapter::notifyBattVoltageOk()");
-//  }
   notifyBattStateAnyChange();
 }
 
 void BatteryAdapter::notifyBattVoltageBelowWarnThreshold()
 {
-//  if (0 != m_trPort)
-//  {
-//    TR_PRINTF(m_trPort, DbgTrace_Level::info, "BatteryAdapter::notifyBattVoltageBelowWarnThreshold()");
-//  }
   notifyBattStateAnyChange();
 }
 
 void BatteryAdapter::notifyBattVoltageBelowStopThreshold()
 {
-//  if (0 != m_trPort)
-//  {
-//    TR_PRINTF(m_trPort, DbgTrace_Level::info, "BatteryAdapter::notifyBattVoltageBelowStopThreshold()");
-//  }
   notifyBattStateAnyChange();
 }
 
 void BatteryAdapter::notifyBattVoltageBelowShutdownThreshold()
 {
-//  if (0 != m_trPort)
-//  {
-//    TR_PRINTF(m_trPort, DbgTrace_Level::info, "BatteryAdapter::notifyBattVoltageBelowShutdownThreshold()");
-//  }
   notifyBattStateAnyChange();
 }
 
 void BatteryAdapter::notifyBattStateAnyChange()
 {
-//  if ((0 != m_trPort) && (0 != m_battery))
+//  if ((0 != m_trPort) && (0 != battery()))
 //  {
 //    TR_PRINTF(m_trPort, DbgTrace_Level::notice, "BatteryAdapter::notifyBattStateAnyChange(), %d.%02dV, %s",
-//              static_cast<int>(m_battery->getBatteryVoltage()),
-//              static_cast<int>(m_battery->getBatteryVoltage()*100.0)-static_cast<int>(m_battery->getBatteryVoltage())*100,
-//              m_battery->isBattVoltageOk() ? "ok" : "/!\\");
+//              static_cast<int>(battery()->getBatteryVoltage()),
+//              static_cast<int>(battery()->getBatteryVoltage()*100.0)-static_cast<int>(battery()->getBatteryVoltage())*100,
+//              battery()->isBattVoltageOk() ? "ok" : "/!\\");
 //  }
 }
 
@@ -209,15 +186,4 @@ void Battery::evaluateBatteryStateAsync()
     m_impl->evaluateStatusAsync();
   }
 }
-
-
-//DbgTrace_Port* Battery::trPort()
-//{
-//  DbgTrace_Port* trPort = 0;
-//  if (0 != m_impl)
-//  {
-//    trPort = m_impl->trPort();
-//  }
-//  return trPort;
-//}
 
